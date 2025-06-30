a75c0c3db371bb7354d567718409cbabe28fcd59
Jisu Shin <69028945+shinj915@users.noreply.github.com>
2025-06-30 16:38:04 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+	int count;
+	scanf("%d", &count);
+	for (int i = 0; i < count; i++) {
+		scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i+1, a, b, a+b);
+	}
+}
